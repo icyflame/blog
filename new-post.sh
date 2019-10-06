bf27f128ce219a9dd0b028a7c38e62b845fa79f3
Siddharth Kannan <mail@siddharthkannan.in>
2019-10-06 16:19:12 +0900

    Add a bash script instead of a perl script for new posts

---
+++ new-post.sh
@@ -0,0 +1,24 @@
+set -xe
+
+post_stub="$1"
+
+if [[ -z "$post_stub" ]]; then
+    echo "ERROR: Must provide at least one argument"
+    echo "Example: ./new-post.sh blog-post-spring"
+    exit 42;
+fi
+
+file_name="`date +%F`-$post_stub.md"
+
+file_path="_posts/$file_name"
+
+touch "$file_path";
+
+cat <<EOF > "$file_path"
+---
+layout: post
+comments: true
+title:
+categories:
+---
+EOF
